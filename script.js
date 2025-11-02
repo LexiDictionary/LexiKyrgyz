e07aca05b57763a442cbc29871e407c427b8323e
Mikko <feidiastp.21@gmail.com>
2025-11-02 15:23:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 function showFilterList(type, value) {
-  // Prevent errors; implement later if needed
 }
 
 function isKyrgyz(text) {
